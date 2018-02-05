ba2735c923172849e052ca622f904bc34f44d5bf
chrisr <c@24.org.uk>
2018-02-05 20:26:05 +0000

    Restructure the CLI code a little

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -9,7 +9,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+    |> parse_args
+    |> process
   end
 
   @doc """
@@ -21,21 +23,33 @@ defmodule Issues.CLI do
   Return a tuple of `{ user, project, count }`, or `:help` if help was given.
   """
   def parse_args(argv) do
-    parse = OptionParser.parse(argv, switches: [ help: :boolean],
+    OptionParser.parse(argv, switches: [ help: :boolean],
       aliases:  [ h:    :help   ])
-    case  parse  do
-
-      { [ help: true ], _, _ }
-      -> :help
-
-      { _, [ user, project, count ], _ }
-      -> { user, project, String.to_integer(count) }
-
-      { _, [ user, project ], _ }
-      -> { user, project, @default_count }
-
-      _ -> :help
-
-    end
+    |> elem(1)
+    |> args_to_internal_representation()
   end
+
+  def args_to_internal_representation([user, project, count]) do
+    { user, project, String.to_integer(count) }
+  end
+
+  def args_to_internal_representation([user, project]) do
+    { user, project, @default_count }
+  end
+
+  def args_to_internal_representation(_) do # bad arg or --help
+    :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage:  issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+
 end
