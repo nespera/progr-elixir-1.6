3dea34ebcaf78e3f222f418199c83a3b7771dd21
chrisr <c@24.org.uk>
2018-02-07 21:41:48 +0000

    Added config

--- issues/lib/issues/github_issues.ex
+++ issues/lib/issues/github_issues.ex
@@ -1,4 +1,8 @@
 defmodule Issues.GithubIssues do
+
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
   @user_agent  [ {"User-agent", "Elixir c@24.org.uk"} ]
 
   def fetch(user, project) do
@@ -8,7 +12,7 @@ defmodule Issues.GithubIssues do
   end
 
   def issues_url(user, project) do
-    "https://api.github.com/repos/#{user}/#{project}/issues"
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
   def handle_response({ _, %{status_code: status_code, body: body}}) do
